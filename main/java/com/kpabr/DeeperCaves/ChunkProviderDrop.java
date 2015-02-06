1852be7337700735b5d00729365f8a6e906788df
Valiec <valiec2019@gmail.com>
2015-02-06 07:15:25 -0500

    Added caverns to top of Drop level Closes #3

--- main/java/com/kpabr/DeeperCaves/ChunkProviderDrop.java
+++ main/java/com/kpabr/DeeperCaves/ChunkProviderDrop.java
@@ -55,7 +55,7 @@ public class ChunkProviderDrop implements IChunkProvider
     private double[] field_147434_q = {};
     private float[] parabolicField = {};
     private double[] stoneNoise = new double[256];
-    private MapGenBase caveGenerator = new MapGenCaves();
+    private MapGenBase caveGenerator = new MapGenDeeperCaves4();
     /** Holds Stronghold Generator */
     private MapGenStronghold strongholdGenerator = new MapGenStronghold();
     /** Holds Village Generator */
@@ -235,7 +235,7 @@ public class ChunkProviderDrop implements IChunkProvider
         this.func_147424_a(par1, par2, ablock);
         this.biomesForGeneration = this.worldObj.getWorldChunkManager().loadBlockGeneratorData(this.biomesForGeneration, par1 * 16, par2 * 16, 16, 16);
         this.replaceBlocksForBiome(par1, par2, ablock, abyte, this.biomesForGeneration);
-        //this.caveGenerator.func_151539_a(this, this.worldObj, par1, par2, ablock);
+        this.caveGenerator.func_151539_a(this, this.worldObj, par1, par2, ablock);
         this.ravineGenerator.func_151539_a(this, this.worldObj, par1, par2, ablock);
 
         if (this.mapFeaturesEnabled)
