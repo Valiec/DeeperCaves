c94b0e48b6846f1b22ab20327ddbf946fa31ff8a
Valiec <valiec2019@gmail.com>
2015-02-11 21:31:19 -0500

    Fixed the biomes

--- main/java/com/kpabr/DeeperCaves/DeeperWorldgen.java
+++ main/java/com/kpabr/DeeperCaves/DeeperWorldgen.java
@@ -34,11 +34,11 @@ public class DeeperWorldgen {
     public int crystalDimID = 9;
     public int compressedDimID = 10;
     public int bedrockPlainsDimID = 11;
-    public BiomeGenDeeper biomeDrop;
-    public BiomeGenDeeper biomeMaze;
-    public BiomeGenDeeper biomeCrystal;
-    public BiomeGenDeeper biomeCompressed;
-    public BiomeGenDeeper biomeBedrockPlains;
+    public BiomeGenDrop biomeDrop;
+    public BiomeGenMaze biomeMaze;
+    public BiomeGenCrystal biomeCrystal;
+    public BiomeGenCompressed biomeCompressed;
+    public BiomeGenBedrockPlains biomeBedrockPlains;
     public void setupWorldgen()
     {
         
@@ -55,23 +55,23 @@ public class DeeperWorldgen {
         DimensionManager.registerProviderType(bedrockPlainsDimID, WorldProviderBedrockPlains.class, true);
         DimensionManager.registerDimension(bedrockPlainsDimID, bedrockPlainsDimID);
         GameRegistry.registerWorldGenerator(deeperblock, 1);
-        this.biomeDrop = new BiomeGenDeeper(80, 0);
+        this.biomeDrop = new BiomeGenDrop(80, 0);
         BiomeEntry dropEntry = new BiomeEntry(this.biomeDrop, 50);
         BiomeDictionary.registerBiomeType(this.biomeDrop, Type.PLAINS);
         BiomeManager.addSpawnBiome(this.biomeDrop);
-        this.biomeMaze = new BiomeGenDeeper(81, 0);
+        this.biomeMaze = new BiomeGenMaze(81, 0);
         BiomeEntry mazeEntry = new BiomeEntry(this.biomeMaze, 50);
         BiomeDictionary.registerBiomeType(this.biomeMaze, Type.PLAINS);
         BiomeManager.addSpawnBiome(this.biomeMaze);
-        this.biomeCrystal = new BiomeGenDeeper(82, 0);
+        this.biomeCrystal = new BiomeGenCrystal(82, 0);
         BiomeEntry crystalEntry = new BiomeEntry(this.biomeCrystal, 50);
         BiomeDictionary.registerBiomeType(this.biomeCrystal, Type.PLAINS);
         BiomeManager.addSpawnBiome(this.biomeCrystal);
-        this.biomeCompressed = new BiomeGenDeeper(83, 0);
+        this.biomeCompressed = new BiomeGenCompressed(83, 0);
         BiomeEntry compressedEntry = new BiomeEntry(this.biomeCompressed, 50);
         BiomeDictionary.registerBiomeType(this.biomeCompressed, Type.PLAINS);
         BiomeManager.addSpawnBiome(this.biomeCompressed);
-        this.biomeBedrockPlains = new BiomeGenDeeper(84, 0);
+        this.biomeBedrockPlains = new BiomeGenBedrockPlains(84, 0);
         BiomeEntry bedrockPlainsEntry = new BiomeEntry(this.biomeBedrockPlains, 50);
         BiomeDictionary.registerBiomeType(this.biomeBedrockPlains, Type.PLAINS);
         BiomeManager.addSpawnBiome(this.biomeBedrockPlains);
