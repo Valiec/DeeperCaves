96ef81bd2852006c7613f4a4e80d0c1935fab872
Valiec <valiec2019@gmail.com>
2015-09-11 10:48:13 -0400

    Finished tweaking spawn egg colors.

--- src/main/java/com/kpabr/DeeperCaves/DeeperMobs.java
+++ src/main/java/com/kpabr/DeeperCaves/DeeperMobs.java
@@ -12,11 +12,11 @@ public class DeeperMobs {
         /*Registering mobs*/
         EntityRegistry.registerGlobalEntityID(EntityDeepZombie.class, "deepZombie", 102, 0x007A7A, 0x365226); //*
         EntityRegistry.addSpawn(EntityDeepZombie.class, 100, 4, 4, EnumCreatureType.monster, DeeperCaves.worldgen.biomeDeepWorld);
-        EntityRegistry.registerGlobalEntityID(EntityDeepSkeleton.class, "deepSkeleton", 103, 0x7A7A7A, 0x000000);
+        EntityRegistry.registerGlobalEntityID(EntityDeepSkeleton.class, "deepSkeleton", 103, 0x8A8A8A, 0x0A0A0A);
         EntityRegistry.addSpawn(EntityDeepSkeleton.class, 100, 4, 4, EnumCreatureType.monster, DeeperCaves.worldgen.biomeDeepWorld);
-        EntityRegistry.registerGlobalEntityID(EntityDeepCaveSpider.class, "deepCaveSpider", 104, 0x00040F, 0x4B0000);
+        EntityRegistry.registerGlobalEntityID(EntityDeepCaveSpider.class, "deepCaveSpider", 104, 0x00141F, 0x5B0000);
         EntityRegistry.addSpawn(EntityDeepCaveSpider.class, 100, 4, 4, EnumCreatureType.monster, DeeperCaves.worldgen.biomeDeepWorld);
-        EntityRegistry.registerGlobalEntityID(EntityDeepCreeper.class, "deepCreeper", 105, 0x006200, 0x000000);
+        EntityRegistry.registerGlobalEntityID(EntityDeepCreeper.class, "deepCreeper", 105, 0x007200, 0x000000);
         EntityRegistry.addSpawn(EntityDeepCreeper.class, 100, 4, 4, EnumCreatureType.monster, DeeperCaves.worldgen.biomeDeepWorld);
         
     }
