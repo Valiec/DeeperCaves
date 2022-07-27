f559b6336226fde0ef927ec3c21f5e72924f3cf5
Valiec <valiec2019@gmail.com>
2022-07-27 18:13:11 -0400

    Initial commit.

---
+++ src/main/java/com/kpabr/deepercaves/mixin/DeeperMixin.java
@@ -0,0 +1,15 @@
+package com.kpabr.deepercaves.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class DeeperMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		com.kpabr.deepercaves.DeeperCaves.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
