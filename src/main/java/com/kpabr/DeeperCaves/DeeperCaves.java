eb9a6c4582959f5b4e088543a2a02e87872a10ae
Valiec <valiec2019@gmail.com>
2024-10-29 15:10:42 -0400

    Version bump + fixed the script

--- src/main/java/com/kpabr/DeeperCaves/DeeperCaves.java
+++ src/main/java/com/kpabr/DeeperCaves/DeeperCaves.java
@@ -51,7 +51,7 @@ public class DeeperCaves
  
     /*Mod ID and Version declarations*/
     public static final String MODID = "DeeperCaves";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "DeeperCaves";
     static int versionID = 11; //Used by version checker!
     
